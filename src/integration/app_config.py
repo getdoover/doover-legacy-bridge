24c19f87ad112df5ade65541741ac591f04de14e
mathsman5133 <mathsman5132@gmail.com>
2025-12-04 18:07:30 +1100

    bump pydoover and change config to be new extended config

--- src/integration/app_config.py
+++ src/integration/app_config.py
@@ -1,36 +1,14 @@
 from pathlib import Path
 
 from pydoover.cloud.processor import IngestionEndpointConfig
-from pydoover.config import Schema, Array, String
-
-
-# fixme: remove this when it's merged into pydoover
-class DeviceString(String):
-    def to_dict(self):
-        data = super().to_dict()
-        data["format"] = "doover-device"
-        return data
-
-class DevicesConfig(Array):
-    def __init__(
-        self,
-        display_name: str = "Devices",
-        *,
-        description: str = "A list of devices to generate reports for.",
-        **kwargs,
-    ):
-        element = DeviceString("Device", pattern="\d+")
-        element._name = "dv_proc_devices"
-        super().__init__(
-            display_name, element=element, description=description, **kwargs
-        )
-        self._name = "dv_proc_devices"
+from pydoover.cloud.processor.config import ExtendedPermissionsConfig
+from pydoover.config import Schema
 
 
 class DooverLegacyBridgeConfig(Schema):
     def __init__(self):
         self.integration = IngestionEndpointConfig()
-        self.devices = DevicesConfig()
+        self.permissions = ExtendedPermissionsConfig()
 
 
 def export():
